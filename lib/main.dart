b1225903b26d77314e2a0a835a8fc3f2787c759d
sernoi25 <jralipe@gmail.com>
2020-10-20 10:48:17 +0800

    start

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:demo_login/models/user.dart';
+import 'package:demo_login/screens/wrapper.dart';
+import 'package:demo_login/services/auth.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:provider/provider.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return StreamProvider<User>.value(
+      value:AuthService().user,
+      child: MaterialApp(
+        home: Wrapper()
+      ),
+    );
+  }
+}
